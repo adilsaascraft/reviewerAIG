f256779d5cd0e4b67c41043a59edd57497126fa1
adilsaascraft <adilsaascraft@gmail.com>
2026-02-03 11:47:22 +0530

    home page updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-export default async function Page() {
-  const data = await fetch('https://api.vercel.app/blog')
-  const posts = await data.json()
-  return (
-    <ul>
-      {posts.map((post) => (
-        <li key={post.id}>{post.title}</li>
-      ))}
-    </ul>
-  )
+import { redirect } from 'next/navigation'
+
+export default function Page() {
+  redirect('/login')
 }
